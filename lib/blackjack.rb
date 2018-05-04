21e1bc9bc3ebe1f7427e1dbc4cf8035294fcd465
jee <jag918@nyu.edu>
2018-05-04 19:36:29 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,54 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  1+Random.rand(10)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_one=deal_card
+  card_two=deal_card
+  sum=card_one+card_two
+  display_card_total(sum)
+  sum
 end
 
-def hit?
-  # code hit? here
+def hit? (current_card_total)
+  prompt_user
+  input=get_user_input
+  if input=='h'
+    card_one=deal_card
+    total=current_card_total+card_one
+    total
+  elsif input =='s'
+    final=current_card_total
+    final
+  else
+    invalid_command
+    prompt_user
+  end
+  
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +56,19 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  current_sum=initial_round
+  current_total = current_sum
+  stay = false
+  until current_total>21 || stay
+    hit_total = hit?(current_total)
+    if hit_total == current_total
+      stay = true
+      current_total = hit?(current_total)
+    else
+      current_total = hit_total
+    end
+    display_card_total(current_total)
+  end
+end_game(current_total)
 end
-    
